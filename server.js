b2603fab22eb7bd064f5ef0d4f6a130be62ddd0c
Banesa Guaderrama <banesa.guaderrama@hotmail.com>
2018-10-20 09:52:20 -0400

    Use environment variable for connection string

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect('mongodb://myproject1:js4567@ds137483.mlab.com:37483/heroku_78t5nvpt'); 
+mongoose.connect(process.env.MOGODB_URI || 'mongodb://localhost/twitter'); 
 
 
 require('./routes/api-routes')(app); 
