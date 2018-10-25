f1aed18da9744387c89ec59126ba784b1d3beaf3
Banesa Guaderrama <banesa.guaderrama@hotmail.com>
2018-10-25 11:19:41 -0400

    Twitter Clone

--- public/app.js
+++ public/app.js
@@ -15,10 +15,11 @@ $('.tweet-btn').on('click', hideModal);
 
 const render = function(tweet){
     $('.center-feed').append(`<p> 
-    <img src="assets/twitter-round-icon.png" class="avatar2" width="48" height="48"><!--small avatar -->
+    <img src="assets/twitter-round-icon.png" class="avatar2" width="48" height="48">
     <div class="user-date">   
-        <span class="userTwitter"> Twitter &nbsp;&nbsp; @Twitter </span> <!-- style this option in css to show on the top of every message-->
-        <span class="postDate"> &nbsp; &nbsp; Oct 18 </span> <!-- style this option in css to show on the top of every message after the Username-->
+        <span class="userTwitter"> Twitter &nbsp;&nbsp; @Twitter </span> 
+        <span class="postDate"> &nbsp; &nbsp; Oct 18 </span> 
+        <button class="delete" data-id="${tweet._id}"><i class="fas fa-times"></i></button>
         <br>
         <br>
         <div class="loadNewTweet">
